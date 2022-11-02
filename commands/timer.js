9889490f793120cdae7b99d4c99b7013d98de88a
tali <73867774+darkoce8@users.noreply.github.com>
2022-11-02 22:23:58 +1000

    Shortened and more readable

--- commands/timer.js
+++ commands/timer.js
@@ -1,7 +1,5 @@
-const { SlashCommandBuilder, SharedSlashCommandOptions } = require('discord.js');
+const { SlashCommandBuilder, SharedSlashCommandOptions, EmbedBuilder } = require('discord.js');
 const wait = require('node:timers/promises').setTimeout;
-const { EmbedBuilder } = require('discord.js');
-
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -29,15 +27,13 @@ module.exports = {
                 .setMinValue(0)),
 
 	async execute(interaction) {
-        const name = interaction.options.getString('name');
-        let name_cap = name[0].toUpperCase() + name.substring(1)
+        const name = (interaction.options.getString('name'))[0].toUpperCase() + (interaction.options.getString('name')).substring(1);
         const days = interaction.options.getInteger('days') ?? 0;
         const hours = interaction.options.getInteger('hours') ?? 0;
         const minutes = interaction.options.getInteger('minutes') ?? 0;
         const seconds = interaction.options.getInteger('seconds') ?? 0;
         const total = days * 86400 + hours * 3600 + minutes * 60 + seconds;
         
-
         function formatTime(seconds) {
             return [
                 parseInt(seconds / 60 / 60),
@@ -51,24 +47,26 @@ module.exports = {
         const formatted = formatTime(seconds);
         const FormattedEmbed = new EmbedBuilder()
 	    .setColor('#2f3136')
-	    .setDescription(`<:clock:1037318080684113932> **|** __Countdown Timer__\n\`${name_cap}\` **»** ${formatted}`)
+	    .setDescription(`<:clock:1037318080684113932> **|** __Countdown Timer__\n\`${name}\` **»** ${formatted}`)
     
 		await interaction.reply({ embeds: [FormattedEmbed] });
 
         // Set I as seconds and minus it by 1 after updating it and waiting 1 second  
         for (i = total-1; i >= 0; i--) {
-            await wait(1000)
             let time = formatTime(i);
+            await wait(1000)
+
             const CountdownEmbed = new EmbedBuilder()
             .setColor('#2f3136')
-            .setDescription(`<:clock:1037318080684113932> **|** __Countdown Timer__\n\`${name_cap}\` **»** ${time}`)
+            .setDescription(`<:clock:1037318080684113932> **|** __Countdown Timer__\n\`${name}\` **»** ${time}`)
+
             await interaction.editReply({ embeds: [CountdownEmbed] })
         }
 
         // Once the countdown has reached 1 second, edit to countdown complete and follow up with a mention.
         const Finished = new EmbedBuilder()
         .setColor('#2f3136')
-        .setDescription(`<:check:1037315609089822751> **|** __Countdown Timer__\n\`${name_cap}\`'s countdown timer has finished.`)
+        .setDescription(`<:check:1037315609089822751> **|** __Countdown Timer__\n\`${name}\`'s countdown timer has finished.`)
         await interaction.editReply({ embeds: [Finished] })
         await interaction.followUp(`<@${interaction.user.id}>`);
 	},
