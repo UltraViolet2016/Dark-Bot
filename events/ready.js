aac0f2a49baeaba4b1ee72edf70ab8361b1d689b
tali <73867774+darkoce8@users.noreply.github.com>
2022-11-02 20:21:15 +1000

    Added Discord Bot Status
    Edited ready event

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,12 @@
 const { version } = require('../config.json');
+const { Client, GatewayIntentBits, ActivityType } = require('discord.js');
 
 module.exports = {
 	name: 'ready',
 	once: true,
 	execute(client) {
 		console.log(`[DarkBot] Successfully logged in ${client.user.tag}. Running version ${version}`);
+		client.user.setActivity("Dark's Server", { type: ActivityType.Watching });
+		client.user.setStatus('dnd');
 	},
 };
