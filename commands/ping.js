dcfcc66f86a031835a4479b9884eb925665927a5
tali <73867774+darkoce8@users.noreply.github.com>
2022-11-02 20:34:55 +1000

    Starting to implement embeds
    First embed in ping.js command

--- commands/ping.js
+++ commands/ping.js
@@ -1,14 +1,17 @@
 const { SlashCommandBuilder } = require('discord.js');
 const wait = require('node:timers/promises').setTimeout;
+const { EmbedBuilder } = require('discord.js');
+
+
+const Pong = new EmbedBuilder()
+	.setColor('#2f3136')
+	.setDescription('Pong!')
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
-		await interaction.reply('Pong');
-		await wait(2000);
-		await interaction.followUp(`@${interaction.user.username}`);
-
+		await interaction.reply({ embeds: [Pong] });
 	},
 };
