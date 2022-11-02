c1174f88259798c9a1414b5a4fa538667729af77
tali <73867774+darkoce8@users.noreply.github.com>
2022-11-02 18:00:31 +1000

    Input Command
    Added an input command that replys with the user input

--- commands/input.js
+++ commands/input.js
@@ -2,13 +2,14 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-	    .setName('test')
+	    .setName('input')
 	    .setDescription('Replies with your input!')
 	    .addStringOption(option =>
 		    option.setName('input')
 			    .setDescription('The input to echo back')
 			    .setRequired(true)),
 	async execute(interaction) {
-		await interaction.reply(`This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`);
+		const message = interaction.options.getString('input') ?? 'No message provided';
+		await interaction.reply(`${message}`);
 	},
 };
