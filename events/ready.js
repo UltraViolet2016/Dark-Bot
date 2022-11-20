d315e8f5351a0c2b7d252b5e4b7ad2a9bba098b9
Dark <73867774+darkoce8@users.noreply.github.com>
2022-11-21 00:06:41 +1000

    Updated config.json
    token is changed nothing is gonna happen if u use it

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,4 @@
-const { version, guildName} = require('../config.json');
+const { version, guildName, botName} = require('../config.json');
 const { ActivityType } = require('discord.js');
 const wait = require('node:timers/promises').setTimeout;
 
@@ -6,11 +6,11 @@ module.exports = {
 	name: 'ready',
 	once: true,
 	async execute(client) {
-		console.log(`[DarkBot] Loading stage complete`);
+		console.log(`[${botName}] Loading stage complete`);
 		await wait(750);
-		console.log(`[DarkBot] Successfully logged into ${client.user.tag}`);
+		console.log(`[${botName}] Successfully logged into ${client.user.tag}`);
 		await wait(750);
-		console.log(`[DarkBot] Running Version ${version}`);
+		console.log(`[${botName}] Running Version ${version}`);
 		client.user.setActivity(`${guildName}`, { type: ActivityType.Watching });
 		client.user.setStatus('dnd');
 
