12945de273b13a94ea9a17920c7ba5baed11913b
Dark <73867774+darkoce8@users.noreply.github.com>
2022-11-20 23:23:36 +1000

    Remove useless folder

--- DarkBot/README.md
+++
@@ -1,5 +0,0 @@
-# DarkBot
-Discord bot written in Javascript
-
-# Test Update
-test updategit
--- DarkBot/events/ready.js
+++
@@ -1,9 +0,0 @@
-const { version } = require('../../config.json');
-
-module.exports = {
-	name: 'ready',
-	once: true,
-	execute(client) {
-		console.log(`[DarkBot] Successfully logged in ${client.user.tag}. Running version ${version}`);
-	},
-};
