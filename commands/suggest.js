3214583204c7fd706f1bbc8999bbd034b08c952b
Dark <73867774+darkoce8@users.noreply.github.com>
2022-12-08 17:21:07 +1000

    Added suggestionchannelid and checkemoji to config.json

--- commands/suggest.js
+++ commands/suggest.js
@@ -1,8 +1,7 @@
-const { SlashCommandBuilder } = require("discord.js");
-const embedMessage = require("../embeds/general/general.js");
+const { SlashCommandBuilder } = require('discord.js');
+const embedMessage = require('../embeds/general/general.js');
 const suggestionEmbed = require('../embeds/suggestions/suggestion.js')
-const suggestionChannelID = require('../config.json');
-const checkEmoji = require('../config.json');
+const { suggestionChannelID, checkEmoji } = require('../config.json'); 
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -12,15 +11,15 @@ module.exports = {
         option.setName('suggestion')
             .setDescription('Enter your suggestion')
             .setRequired(true)),
-    
+
     async execute(interaction) {
         const user = interaction.user.tag
         const suggestion = interaction.options.getString('suggestion');
         const avatar = interaction.user.displayAvatarURL();
 
-        interaction.reply({embeds: [embedMessage(`<:check:1037315609089822751> **|** Suggestion has been successfully posted in <#1047032593767473232>`)] });
+        interaction.reply({embeds: [embedMessage(`${checkEmoji} **|** Suggestion has been successfully posted in <#${suggestionChannelID}>`)] });
 
-        const message = await interaction.guild.channels.cache.get('1047032593767473232').send({ embeds: [suggestionEmbed(user, avatar, suggestion)] });
+        const message = await interaction.guild.channels.cache.get(suggestionChannelID).send({ embeds: [suggestionEmbed(user, avatar, suggestion)] });
         message.react('👍')
         .then(() => message.react('👎'));
     }
--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,4 @@
-const { version, guildName, botName} = require('../config.json');
+const { version, guildName, botName } = require('../config.json');
 const { ActivityType } = require('discord.js');
 const wait = require('node:timers/promises').setTimeout;
 
